b831a9ec9d65c5a4355919839f4faa7c38360284
karadenizemirr <karadenizemirr@gmail.com>
2023-07-17 19:37:45 +0300

    modified:   src/helper/db.ts

--- src/helper/db.ts
+++ src/helper/db.ts
@@ -9,33 +9,33 @@ database and manage database operations. */
 /* Kod, `DataSource` sınıfının yeni bir örneğini oluşturuyor ve bunu
 AppDataSource` sabiti. DataSource` sınıfı tipik olarak TypeORM`da bir veri kaynağına bağlanmak için kullanılır
 amacı veritabanı ve veritabanı işlemlerini yönetmek. */
-export const AppDataSource = new DataSource({
-    type: "mysql",
-     host: "localhost",
-     port: 3306,
-     username: "administrator",
-     password: "123456",
-     database: "kutaykoca",
-     entities: [Location, Station],
-     migrations: [__dirname + "/migrations/**/*{.ts,.js}"],
-     synchronize: true,
-     logging: true,
-     subscribers: [],
-})
-
 // export const AppDataSource = new DataSource({
 //     type: "mysql",
-//      host: "db-mysql-sfo3-96391-do-user-9457349-0.b.db.ondigitalocean.com",
-//      port: 25060,
-//      username: "doadmin",
-//      password: "AVNS_eaRHq9pmkLdsEiMAK-l",
+//      host: "localhost",
+//      port: 3306,
+//      username: "administrator",
+//      password: "123456",
 //      database: "kutaykoca",
 //      entities: [Location, Station],
 //      migrations: [__dirname + "/migrations/**/*{.ts,.js}"],
 //      synchronize: true,
 //      logging: true,
 //      subscribers: [],
-//      extra: {
-//         ssl: false
-//      }
 // })
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+     host: "db-mysql-sfo3-96391-do-user-9457349-0.b.db.ondigitalocean.com",
+     port: 25060,
+     username: "doadmin",
+     password: "AVNS_eaRHq9pmkLdsEiMAK-l",
+     database: "kutaykoca",
+     entities: [Location, Station],
+     migrations: [__dirname + "/migrations/**/*{.ts,.js}"],
+     synchronize: true,
+     logging: true,
+     subscribers: [],
+     extra: {
+        ssl: false
+     }
+})
