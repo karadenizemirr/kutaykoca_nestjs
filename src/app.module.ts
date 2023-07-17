fd0a2d03f545c522a9976ecfe884e68351648aff
karadenizemirr <karadenizemirr@gmail.com>
2023-07-17 05:29:29 +0300

    Update - 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LocationController } from './controller/location/location.controller';
-import { LocationModule } from './controller/location/location.module';
 import { ScraperService } from './services/scraper.service';
 import { KnnService } from './services/knn.service';
-import { StationModule } from './controller/station/station.module';
 import { StationController } from './controller/station/station.controller';
 
 @Module({
-  imports: [LocationModule, StationModule],
+  imports: [],
   controllers: [AppController, LocationController, StationController],
   providers: [AppService,ScraperService, KnnService],
 })
