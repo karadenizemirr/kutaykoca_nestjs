8b684442a9c4fd39b0c3ba2b12645ec8af3b880b
karadenizemirr <karadenizemirr@gmail.com>
2023-07-17 04:45:55 +0300

    Update - 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,11 @@ import { LocationModule } from './controller/location/location.module';
 import { ScraperService } from './services/scraper.service';
 import { KnnService } from './services/knn.service';
 import { StationModule } from './controller/station/station.module';
+import { StationController } from './controller/station/station.controller';
 
 @Module({
   imports: [LocationModule, StationModule],
-  controllers: [AppController, LocationController],
+  controllers: [AppController, LocationController, StationController],
   providers: [AppService,ScraperService, KnnService],
 })
 export class AppModule {}
